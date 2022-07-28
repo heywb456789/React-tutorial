ad4045b0aec5dd809057b2d2599dbe51de459ad8
wooBinKim <wbkim@texture.kr>
2022-07-28 23:29:04 +0900

    Component Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+  const expense = [
+    { title: "Toilet Paper", amount: 10, date: new Date(2021, 3, 12) },
+    { title: "Pencil", amount: 20.67, date: new Date(2021, 4, 12) },
+    { title: "CarInsurance", amount: 294.67, date: new Date(2021, 5, 12) },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>gogogo</h2>
+      <Expenses items={expense} />
     </div>
   );
-}
+};
 
 export default App;
